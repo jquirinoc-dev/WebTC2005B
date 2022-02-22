2f36bc80ef7bfe2f9b8df92a6537e685df33c938
jonathan619 <jonathanquirino02@gmail.com>
2022-02-22 08:47:45 -0600

    ola soy un commit

---
+++ script.js
@@ -0,0 +1,23 @@
+//aqui adentro va javascript
+
+//declarar variables
+x = 5;
+x = "I hate u";
+
+console.log("HOLA DESDE AFUERA DE LA FUNCTION");
+
+function example(){
+    
+    console.log("Hola a todos jiji");
+    alert("Hola a todos jiji");
+}
+
+function boton2(){
+    alert("BOTON 2");
+}
+
+function boton3(){
+    alert("BOTON 3");
+    
+    document.getElementById("parrafitobonito").innerHTML = "<h1>CAMBIE PARA SIEMPRE.</h1>";
+}
