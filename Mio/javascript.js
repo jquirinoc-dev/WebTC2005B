598e0368d880517423766933ad3cf8e338992b96
jonathan619 <jonathanquirino02@gmail.com>
2022-02-23 07:38:07 -0600

    Reacomodo

---
+++ Mio/javascript.js
@@ -0,0 +1,18 @@
+alert('Hello World, this is Javascript from an External File!');
+
+var x = 0;
+
+/* console.log("x es igual a " + x);
+x++;
+console.log("x++ es igual a " + x);
+x++
+console.log("x++ una vez mas es igual a " + x); */
+
+myFunction();
+myFunction();
+myFunction();
+
+function myFunction(){
+    x++
+    console.log("Hello " + x);
+}
