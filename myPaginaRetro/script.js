ddc4606fbdf66130a66b91e7dd8fd43f857a95ba
jonathan619 <jonathanquirino02@gmail.com>
2022-03-11 07:07:53 -0600

    callese pues

--- myPaginaRetro/script.js
+++ myPaginaRetro/script.js
@@ -1,3 +1,5 @@
+x = 181221;
+
 function mostrarFormYVaciar(){
     alert("Parece ser que tu nombre completo es: " + document.getElementById("nombre").value
     + " " + document.getElementById("segundonombre").value + " " + document.getElementById("apellido").value + "\n"
@@ -6,4 +8,11 @@ function mostrarFormYVaciar(){
     document.getElementById("nombre").value = "";
     document.getElementById("segundonombre").value = "";
     document.getElementById("apellido").value = "";
+}
+
+const persona = {
+    nombre: "Saachi Hernandez",
+    edad: 20,
+    estatura: 154,
+    sexo: femenino
 }
