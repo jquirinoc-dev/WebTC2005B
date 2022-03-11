0efd46f28c1ce00a87e0bba7f7dc840c1d778093
jonathan619 <jonathanquirino02@gmail.com>
2022-03-11 08:59:44 -0600

    POSTs, GETs, endpoints

--- react-node-app/server/index.js
+++ react-node-app/server/index.js
@@ -6,6 +6,39 @@ const PORT = process.env.PORT || 3001;
 
 const app = express();
 
+const fs = require("fs");
+
+const newPet = {
+    "pet4" : {
+        "name" : "nala",
+        "type" : "dog",
+        "owner" : "jonathan",
+        "color" : "salt and pepper",
+        "id" : 4
+    }
+}
+
+app.post("/addPet", (req, res) => {
+    fs.readFile(__dirname + "/" + "pets.json", "utf8", (err, data) => {
+        data = JSON.parse(data);
+        data["pet4"] = newPet["pet4"];
+        console.log(data);
+        res.end(JSON.stringify(data));
+    });
+});
+
+
+app.get("/api", (req, res) => {
+    res.json({message: "Hello from server side!" });
+});
+
+app.get("/pets", (req, res) => {
+    fs.readFile(__dirname + "/" + "pets.json", "utf-8", (err, data) => {
+        console.log(data);
+        res.end(data);
+    });
+});
+
 app.listen(PORT, () => {
     console.log(`Server listening on ${PORT}`);
 });
